4ae81588015a1842d255c8edf69732f98b97ff80
Ravindra <sirviravindra609@gmail.com>
2024-05-09 21:04:54 +0530

    chore: Add Next.js configuration, README, and gitignore files
    feat: Add imagebox and video popup components
    
    chore: Add plugins and layout files
    
    feat: Add not-found page and sticky header hook
    
    chore: Add timeline styles
    
    feat: Add MarqueeAreaHomeTwo component

---
+++ src/app/layout.tsx
@@ -0,0 +1,24 @@
+
+import "@/styles/index.scss";
+
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+        <head>
+        <link rel="icon" href="/assets/img/favicon.svg" />
+        <link
+          rel="stylesheet"
+          href="https://fonts.googleapis.com/css2?family=Inter+Tight:wght@400;500;600;700;900&family=Kanit:wght@400;500;600;700&display=swap"
+        />
+        <title>Vixan - Digital  Creative Agency Next js Template</title>
+      </head>
+
+      <body>{children}</body>
+    </html>
+  );
+}
