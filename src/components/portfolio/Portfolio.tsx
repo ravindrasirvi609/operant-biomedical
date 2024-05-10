d2d3297a9ab1aff477f0aeefd66cf31e8381f4eb
Ravindra <sirviravindra609@gmail.com>
2024-05-10 06:46:23 +0530

    chore: Update portfolio images and titles

--- src/components/portfolio/Portfolio.tsx
+++ src/components/portfolio/Portfolio.tsx
@@ -1,17 +1,16 @@
-'use client'
+"use client";
 
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // import portfolio_data from '@/data/portfolio_data';
-import Link from 'next/link';
-import Image, { StaticImageData } from 'next/image';
+import Link from "next/link";
+import Image, { StaticImageData } from "next/image";
 
 import portfolio_img_6 from "@/assets/img/portfolio_1.jpg";
 import portfolio_img_7 from "@/assets/img/portfolio_2.jpg";
 import portfolio_img_8 from "@/assets/img/portfolio_3.jpg";
 import portfolio_img_9 from "@/assets/img/portfolio_5.jpg";
 import portfolio_img_10 from "@/assets/img/portfolio_6.jpg";
-import portfolio_img_11 from "@/assets/img/portfolio_7.jpg"; 
-
+import portfolio_img_11 from "@/assets/img/portfolio_7.jpg";
 
 interface DataType {
   id: number;
@@ -23,55 +22,57 @@ interface DataType {
 
 const portfolio_data: DataType[] = [
   // portfolio pages
-	{
-		id: 1,
-		category: 'UI Design',
-		img: portfolio_img_6,
-		title: "Project Task Management",
-		des: "Digital Services / App Design",
-	},
-	{
-		id: 2,
-		category: 'UX Design',
-		img: portfolio_img_7,
-		title: "Project Task Management",
-		des: "Digital Services / App Design",
-	},
-	{
-		id: 3,
-		category: 'Development',
-		img: portfolio_img_8,
-		title: "Project Task Management",
-		des: "Digital Services / App Design",
-	},
-	{
-		id: 4,
-		category: 'Finance',
-		img: portfolio_img_9,
-		title: "Project Task Management",
-		des: "Digital Services / App Design",
-	},
-	{
-		id: 5,
-		category: 'UX Design',
-		img: portfolio_img_10,
-		title: "Project Task Management",
-		des: "Digital Services / App Design",
-	},
-	{
-		id: 6,
-		category: 'UI Design',
-		img: portfolio_img_11,
-		title: "Project Task Management",
-		des: "Digital Services / App Design",
-	},
-]
+  {
+    id: 1,
+    category: "Pharma Reserch Publications",
+    img: portfolio_img_6,
+    title: "Project Task Management",
+    des: "pharmacy research and ",
+  },
+  {
+    id: 2,
+    category: "UX Design",
+    img: portfolio_img_7,
+    title: "Project Task Management",
+    des: "Digital Services / App Design",
+  },
+  {
+    id: 3,
+    category: "Development",
+    img: portfolio_img_8,
+    title: "Project Task Management",
+    des: "Digital Services / App Design",
+  },
+  {
+    id: 4,
+    category: "Finance",
+    img: portfolio_img_9,
+    title: "Project Task Management",
+    des: "Digital Services / App Design",
+  },
+  {
+    id: 5,
+    category: "UX Design",
+    img: portfolio_img_10,
+    title: "Project Task Management",
+    des: "Digital Services / App Design",
+  },
+  {
+    id: 6,
+    category: "UI Design",
+    img: portfolio_img_11,
+    title: "Project Task Management",
+    des: "Digital Services / App Design",
+  },
+];
 
 // data
-const categories = ["All", ...new Set(portfolio_data.map((item) => item.category))];
+const categories = [
+  "All",
+  ...new Set(portfolio_data.map((item) => item.category)),
+];
 
 const Portfolio = () => {
-
   const [activeCategory, setActiveCategory] = useState("All");
   const [items, setItems] = useState(portfolio_data);
 
@@ -86,7 +87,6 @@ const Portfolio = () => {
       setItems(findItems);
     }
   };
-
 
   return (
     <>
@@ -104,7 +104,6 @@ const Portfolio = () => {
         </div>
       </section>
 
-
       <div className="cs_height_50 cs_height_lg_25"></div>
       <section className="cs_ui_design">
         <div className="container">
@@ -119,7 +118,7 @@ const Portfolio = () => {
                         className={`${cate === activeCategory ? "active" : ""}`}
                       >
                         {cate}
-                      </li>  {' '}
+                      </li>{" "}
                     </React.Fragment>
                   ))}
                 </ul>
@@ -127,31 +126,34 @@ const Portfolio = () => {
             </div>
             <div className="cs_isotop_items_details row">
               {items.map((item, i) => (
-                <div key={i} className="col-md-4 cs_item cs_ui_design cs_development">
-                  <Link href="/portfolio-details" className="cs_portfolio cs_style_1">
+                <div
+                  key={i}
+                  className="col-md-4 cs_item cs_ui_design cs_development"
+                >
+                  <Link
+                    href="/portfolio-details"
+                    className="cs_portfolio cs_style_1"
+                  >
                     <div className="cs_portfolio_img">
                       <Image src={item.img} className="img-fluid" alt="Thumb" />
                     </div>
                     <div className="cs_portfolio_overlay"></div>
                     <div className="cs_portfolio_info">
-                      <h2 className="cs_portfolio_title">
-                        {item.title}
-                      </h2>
-                      <div className="cs_portfolio_subtitle">
-                        {item.des}
-                      </div>
+                      <h2 className="cs_portfolio_title">{item.title}</h2>
+                      <div className="cs_portfolio_subtitle">{item.des}</div>
                     </div>
                   </Link>
                 </div>
-              ))} 
-
+              ))}
             </div>
           </div>
           <div className="cs_height_70 cs_height_lg_30"></div>
           <div>
             <div className="cs_hero_btn_wrap text-center">
               <div className="cs_round_btn_wrap">
-                <a href="#" className="cs_hero_btn cs_round_btn btn-item"><span></span> Load More</a>
+                <a href="#" className="cs_hero_btn cs_round_btn btn-item">
+                  <span></span> Load More
+                </a>
               </div>
             </div>
           </div>
--- src/components/portfolio/PortfolioHomeOne.tsx
+++ src/components/portfolio/PortfolioHomeOne.tsx
@@ -1,53 +1,53 @@
-'use client'
-import React from 'react';
-import Link from 'next/link';
-import Image, { StaticImageData } from 'next/image';
+"use client";
+import React from "react";
+import Link from "next/link";
+import Image, { StaticImageData } from "next/image";
 
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import portfolio_img_1 from '@/assets/img/portfolio_1.jpg';
-import portfolio_img_2 from '@/assets/img/portfolio_2.jpg';
-import portfolio_img_3 from '@/assets/img/portfolio_3.jpg';
+import portfolio_img_1 from "@/assets/img/portfolio_1.jpg";
+import portfolio_img_2 from "@/assets/img/portfolio_2.jpg";
+import portfolio_img_3 from "@/assets/img/portfolio_3.jpg";
 
 interface DataType {
   img: StaticImageData;
   title: string;
   category: string;
-}[]
+}
+[];
 
 const portfolio_slider: DataType[] = [
   {
     img: portfolio_img_1,
-    title: `Project Task Management`,
-    category: `Digital Services / App Design`,
+    title: "Exploring Novel Drug Targets",
+    category: "Pharma Research Publications",
   },
   {
     img: portfolio_img_2,
-    title: `Project Task Management`,
-    category: `Digital Services / Web Design`,
+    title: "Clinical Trials Advancements",
+    category: "Pharma Research Publications",
   },
   {
     img: portfolio_img_3,
-    title: `Project Task Management`,
-    category: `Digital Services / Figma Design`,
+    title: "Precision Medicine Innovations",
+    category: "Pharma Research Publications",
   },
   {
     img: portfolio_img_1,
-    title: `Project Task Management`,
-    category: `Digital Services / App Design`,
+    title: "Drug Repurposing Strategies",
+    category: "Pharma Research Publications",
   },
   {
     img: portfolio_img_2,
-    title: `Project Task Management`,
-    category: `Digital Services / Web Design`,
+    title: "Biopharmaceutical Manufacturing Trends",
+    category: "Pharma Research Publications",
   },
   {
-    img: portfolio_img_3,
-    title: `Project Task Management`,
-    category: `Digital Services / Figma Design`,
+    img: portfolio_img_2,
+    title: "Regulatory Compliance Updates",
+    category: "Pharma Research Publications",
   },
-]
-
+];
 
 const PortfolioHomeOne = () => {
   return (
@@ -70,33 +70,32 @@ const PortfolioHomeOne = () => {
         <Swiper
           loop={true}
           speed={1000}
-          slidesPerView="auto" 
+          slidesPerView="auto"
           pagination={{
             el: ".cs_pagination",
             clickable: true,
           }}
-          className="cs_horizontal_scrolls anim_div_ShowDowns">
-          {portfolio_slider.map((item, i) =>
+          className="cs_horizontal_scrolls anim_div_ShowDowns"
+        >
+          {portfolio_slider.map((item, i) => (
             <SwiperSlide key={i} className="swiper-slide">
               <div className="cs_horizontal_scroll">
-                <Link href="/portfolio-details" className="cs_portfolio cs_style_1">
+                <Link
+                  href="/portfolio-details"
+                  className="cs_portfolio cs_style_1"
+                >
                   <div className="cs_portfolio_img">
                     <Image src={item.img} alt="Thumb" />
                   </div>
                   <div className="cs_portfolio_overlay"></div>
                   <div className="cs_portfolio_info">
-                    <h2 className="cs_portfolio_title">
-                      {item.title}
-                    </h2>
-                    <div className="cs_portfolio_subtitle">
-                      {item.category}
-                    </div>
+                    <h2 className="cs_portfolio_title">{item.title}</h2>
+                    <div className="cs_portfolio_subtitle">{item.category}</div>
                   </div>
                 </Link>
               </div>
             </SwiperSlide>
-          )} 
-
+          ))}
         </Swiper>
       </div>
       <div className="cs_height_145 cs_height_lg_60"></div>
