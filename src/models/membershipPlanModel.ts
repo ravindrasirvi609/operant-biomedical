3f33c7d58d33a6eecf6df526a96c995a8b750f9b
Ravindra <sirviravindra609@gmail.com>
2024-05-10 21:53:41 +0530

    feat: Add membershipPlanModel to handle membership plans

---
+++ src/models/membershipPlanModel.ts
@@ -0,0 +1,40 @@
+import mongoose from "mongoose";
+const { Schema } = mongoose;
+
+const membershipPlanSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+    duration: {
+      type: Number,
+      required: true,
+    },
+    maxAllowedBooks: {
+      type: Number,
+      required: true,
+    },
+    discount: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Membership =
+  mongoose.models.membershipPlan ||
+  mongoose.model("membershipPlan", membershipPlanSchema);
+
+export default Membership;
