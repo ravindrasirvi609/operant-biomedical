b5f585219705380857e86e5b17e25d73feb2af98
Ravindra <sirviravindra609@gmail.com>
2024-05-22 13:06:26 +0530

    chore: Update next.config.mjs to include new image domain

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: false,
-   images: {
-    domains: ["unsplash.com", "images.unsplash.com", "plus.unsplash.com" ,"aceternity.com", "res.cloudinary.com", "cdn.pixabay.com", "cdn.dribbble.com", "cdn.fakercloud.com", "cdn.iconscout", "www.youtube.com"],
+        images: {
+    domains: ["unsplash.com", "images.unsplash.com", "plus.unsplash.com" ,"aceternity.com", "res.cloudinary.com", "placehold.co", "cdn.dribbble.com", "cdn.fakercloud.com", "cdn.iconscout", "www.youtube.com"],
   },
-};
+}
+
+export default nextConfig
