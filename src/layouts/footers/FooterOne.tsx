c222859d84f971ee2b31e86883c90a4f541ecdc9
Ravindra <sirviravindra609@gmail.com>
2024-05-25 14:39:48 +0530

    refactor: Update footer logo image in FooterOne component

--- src/layouts/footers/FooterOne.tsx
+++ src/layouts/footers/FooterOne.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Link from "next/link";
 import Image from "next/image";
-import footer_logo from "@/assets/img/footer_logo.svg";
+import footer_logo from "@/assets/img/Logo_white.png";
 import { CopyRight, SocialLinks } from "@/components/common/SocialLinks";
 
 interface DataType {
