da2bd55fed1a0e271bd44ad4a4b8f6d8a89a0b93
Ravindra <sirviravindra609@gmail.com>
2024-05-25 15:31:08 +0530

    refactor: Update portfolio image file name in PortfolioHomeOne component

--- src/components/portfolio/PortfolioHomeOne.tsx
+++ src/components/portfolio/PortfolioHomeOne.tsx
@@ -7,7 +7,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 import portfolio_img_1 from "@/assets/img/portfolio/Pharmanecia6_E.png";
 import portfolio_img_2 from "@/assets/img/portfolio/Pharmanecia1_E.png";
-import portfolio_img_3 from "@/assets/img/portfolio/Pharmanecia2_E.png";
+import portfolio_img_3 from "@/assets/img/portfolio/pharmanecia2_E.png";
 import portfolio_img_4 from "@/assets/img/portfolio/Pharmanecia3_E.png";
 import portfolio_img_5 from "@/assets/img/portfolio/Pharmanecia4_E.png";
 import portfolio_img_6 from "@/assets/img/portfolio/Pharmanecia5_E.png";
